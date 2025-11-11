f24fbf96cd308bbf527cba400312d572defd0b11
Bhavya Bajpai <bhavyabajpai79@gmail.com>
2025-11-11 21:28:20 +0530

    still work left

--- src/How_It_Works.jsx
+++ src/How_It_Works.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AnimatedContent from './components/AnimatedContent'
 
 const points = [
   {
@@ -35,35 +36,46 @@ const points = [
 
 const How_It_Works = () => {
   return (
-    <section id='how_works' className='py-20 bg-gray-50'>
-      <div className='container mx-auto px-6 text-center'>
+    <section id='how_works' className='py-20 bg- flex justify-center'>
+      <AnimatedContent
+              
+              distance={100} 
+              direction="vertical"
+              duration={1.0}
+              ease="power2.out" 
+              initialOpacity={.1}
+              animateOpacity
+              scale={.95}
+              threshold={0.3} 
+            >
+      <div className='text-center max-w-7xl mx-auto px-6 text-center'>
         
-        <h2 className='text-[40px] font-bold'>
+        <h2 className='text-[40px] font-bold mb-3 md:mb-8 text-cyan-500'>
           How It Works
         </h2>
-        <p className=''>
+        <p className='text-blue-100 mb-16'>
           Explore our powerful features designed to make your coding experience smoother and more efficient.
         </p>
 
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-9'>
+        <div className='grid grid-cols-1 md:grid-cols-2  gap-12'>
           
           {points.map((feature, index) => (
             <div
               key={index}
               className='
-                group relative bg-white rounded-xl shadow-lg p-6 
+                group relative bg-[#1b3553] rounded-2xl  px-3 py-10  max-w-6xl 
                 flex flex-col items-center 
                 transition-all duration-300 ease-in-out transform 
-                hover:-translate-y-2 hover:shadow-2xl hover:scale-105
-                overflow-hidden border border-gray-100
+                hover:translate-y-2 hover:shadow-gray-800 hover:shadow-2xl hover:scale-110
+                overflow-hidden border border-gray-600 text-blue-200
               '
             >
-              <div className='flex flex-col items-center text-center transition-opacity duration-300 group-hover:opacity-0'>
-                <div className='text-5xl mb-4 p-4  rounded-full'>
+              <div className='flex flex-col items-center text-center transition-opacity p-5 duration-300 group-hover:opacity-0'>
+                {/* <div className='text-5xl mb-4 p-4  rounded-full'>
                   {feature.icon}
-                </div>
-                <h3 className='text-xl font-bold text-gray-800 mb-10'>{feature.title}</h3>
-                <p className='text-gray-500'>
+                </div> */}
+                <h3 className='text-xl font-bold text-[#5dbbee] p-4 text- mb-4'>{feature.title}</h3>
+                <p className='text-blue-200 text-base'>
                   {feature.shortDescription}
                 </p>
               </div>
@@ -71,7 +83,7 @@ const How_It_Works = () => {
               <div 
                 className='
                   absolute inset-0 p-6 flex items-center justify-center 
-                  opacity-0 transition-opacity duration-300 
+                  opacity-0 transition-opacity duration-300 bg-cyan-[#1b627e]
                   group-hover:opacity-100
                 '
               >
@@ -82,6 +94,7 @@ const How_It_Works = () => {
 
         </div>
       </div>
+      </AnimatedContent>
     </section>
   );
 };
